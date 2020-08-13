c61fbc7ac8f0c1f7d136d16f3aa14cc3e7755eab
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-13 09:32:53 +0300

    added project

---
+++ LinqBasic/LinqBasic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
